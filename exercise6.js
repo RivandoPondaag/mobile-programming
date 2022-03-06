3f5dd4ae6003987956b37324074f5c4761a22392
palalangan <s21910284@student.unklab.ac.id>
2022-03-07 02:08:31 +0900

    Menambahkan penjelasan pada state

--- exercise6.js
+++ exercise6.js
@@ -1,18 +1,15 @@
-import { SafeAreaView, View, Text,TouchableOpacity } from 'react-native'
-import React,{useState} from 'react'
-
+import { SafeAreaView, View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
 
 // export default function App() {
 //   return (
 //     <SafeAreaView>
 //     <Tambah props={"Tambah"}/>
 //     <Nilai props={'0'}/>
-//      <Kurang props={"Kurang"}/>  
+//      <Kurang props={"Kurang"}/>
 //     </SafeAreaView>
 //   )
 // }
-
-
 
 // export function Tambah({props}) {
 //   return (
@@ -38,25 +35,35 @@ import React,{useState} from 'react'
 //   )
 // }
 
-
 export default function App() {
-const[nilai, rubahNilai]=useState(0)
+  //cara menggunakan state
+  //state tidak dapat berpindah component tetapi bisa berubah nilai
+  const [nilai, rubahNilai] = useState(0);
 
   return (
     <SafeAreaView>
-      <TouchableOpacity onPress={()=>rubahNilai(nilai+1)}> 
+      <TouchableOpacity onPress={() => rubahNilai(nilai + 1)}>
+        <View>
+          <Text style={{ fontSize: 40, color: "blue", alignSelf: "center" }}>
+            {" "}
+            Tambah{" "}
+          </Text>
+        </View>
+      </TouchableOpacity>
       <View>
-       <Text style={{ fontSize: 40, color: 'blue', alignSelf: 'center' }}> Tambah </Text>
-     </View>
-     </TouchableOpacity>
-      <View>
-       <Text style={{ fontSize: 40, color: 'black', alignSelf: 'center' }}> {nilai} </Text>
-     </View>
-     <TouchableOpacity onPress={()=>rubahNilai(nilai-1)}>
-      <View>
-       <Text style={{ fontSize: 40, color: 'blue', alignSelf: 'center' }}> Kurang</Text>
-     </View>
-     </TouchableOpacity>
+        <Text style={{ fontSize: 40, color: "black", alignSelf: "center" }}>
+          {" "}
+          {nilai}{" "}
+        </Text>
+      </View>
+      <TouchableOpacity onPress={() => rubahNilai(nilai - 1)}>
+        <View>
+          <Text style={{ fontSize: 40, color: "blue", alignSelf: "center" }}>
+            {" "}
+            Kurang
+          </Text>
+        </View>
+      </TouchableOpacity>
     </SafeAreaView>
-  )
+  );
 }
