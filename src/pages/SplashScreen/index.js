5a2a5f4034318024fcc41eb853de14a892d4350f
palalangan <s21910284@student.unklab.ac.id>
2022-04-18 23:04:56 +0900

    Membuat Splash Screen

---
+++ src/pages/SplashScreen/index.js
@@ -0,0 +1,28 @@
+import {StyleSheet, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {Logo} from '../../assets';
+
+const SplashScreen = ({navigation}) => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.replace('SignIn');
+    }, 3000);
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Logo />
+    </View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#069A8E',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
